ff7340f2750ab31e3489bc0654aa60702bdf9f52
ManojNambiraj <postboxmano@gmail.com>
2025-06-09 19:47:39 +0530

    JS intro

---
+++ JS/intro/script.js
@@ -0,0 +1,23 @@
+// Javascript
+
+// ES -> Ecma script
+
+// Old version: ES5
+// New version: ES6
+
+// Variables:
+
+    // myAgeInSchoolTime10thStd = 5
+
+    // console.log(myAgeInSchoolTime10thStd);
+
+// Datatypes:
+
+    // myage = 20
+    // myage = "Twenty"
+    // myage = false
+    // myage = null
+    // let myage
+
+    // console.log(myage, typeof(myage));
+
