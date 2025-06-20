b6a99ec5dbb10730dbe4a67c2ff2fc312905c43b
ManojNambiraj <postboxmano@gmail.com>
2025-06-20 19:15:41 +0530

    JS Functions

---
+++ JS/class5/script.js
@@ -0,0 +1,60 @@
+// Functions
+
+    // 1. Built-in functions
+    // 2. User defined functions
+
+    // function demo(){              // functions def
+    //     for(let i = 0; i <= 5; i++){
+    //         console.log(i);
+    //     }
+    // }
+
+    // console.log("Good morning..!");
+
+    // demo();                      // function call
+
+    // // DRY --> Don't repeat your code
+
+    // console.log("Good afternoon..!");
+
+    // demo();   
+
+    // console.log("Good night");
+
+    // demo();  
+
+// Normal function
+
+    // function demo(a, b){
+    //     return a * b
+    // }
+
+    // const result = demo(100, 2)
+
+    // console.log(result);
+
+// Ananymous function
+
+    // student = {
+    //     name: "ravi",
+    //     age: 20,
+    //     address: function(n){
+    //         console.log(n);
+    //     }
+    // }
+
+    // console.log(student.address("hello"));
+
+// Arrow function
+
+    // const mul = (a, b) => a * b
+    // const add = (a, b) => a + b
+
+    // console.log(mul(5, 3));
+    // console.log(add(5, 3));
+
+// IIFE (Immediatly Invoked Function Expression)
+
+    // (() => {
+    //     console.log("hi");
+    // })()
