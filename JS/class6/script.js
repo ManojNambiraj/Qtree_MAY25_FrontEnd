28bd9c3d66140cc20e68f689200444702259dcb4
ManojNambiraj <postboxmano@gmail.com>
2025-06-23 19:58:20 +0530

    Array methods

---
+++ JS/class6/script.js
@@ -0,0 +1,43 @@
+// Array Methods:  MRF Methods
+
+    // M -> map
+    // R -> Reduce
+    // F -> Filter
+
+    // let arr = [1, 5, 7, 8, 50, 81, 99, 107, 222]
+
+    // for(let i = 0; i < arr.length; i++){
+    //     console.log(arr[i])
+    // }
+
+// map:
+
+    // let result = arr.map((item) => {
+    //     return item * 2;
+    // })
+
+    // console.log(result);
+
+// filter
+
+    // let result = arr.filter((item) => item % 2 != 0)
+
+    // console.log(result);
+
+// reduce
+
+    // let arr = [1, 5, 7, 8, 50, 81, 99, 107, 222];
+
+    // let result = arr.reduce((prev, item) => prev + item)
+
+    // console.log(result);
+
+// Exmples of Callback functions
+
+    // setTimeout(() => {
+    //   console.log("hello");
+    // }, 3000);
+
+    // setInterval(() => {
+    // console.log("hello");
+    // }, 4000);
